cb6e241c955a44c9e08ce51b2e6439793f37f467
Adam La Morre <alamorre@gmail.com>
2019-09-11 19:38:00 -0400

    Ready to hind on expanded video

--- event/src/reducers/focus.js
+++ event/src/reducers/focus.js
@@ -6,8 +6,10 @@ export default (state = initialState, action) => {
       // If we're now focusing, inject CSS
       if(!state){
         chrome.tabs.insertCSS(null, { code: "ytd-watch-next-secondary-results-renderer{ display: none !important; }", allFrames: true }, function() { console.log('Focus style on.') })
+        chrome.tabs.insertCSS(null, { code: "ytd-browse{ display: none !important; }", allFrames: true }, function() { console.log('Focus style on.') })
       } else {
         chrome.tabs.insertCSS(null, { code: "ytd-watch-next-secondary-results-renderer{ display: flex !important; }", allFrames: true }, function() { console.log('Focus style off.') })
+        chrome.tabs.insertCSS(null, { code: "ytd-browse{ display: flex !important; }", allFrames: true }, function() { console.log('Focus style on.') })
       }
       // Return the new state
       return !state;
